ab90218e6ebb568fadd8f6862fb03d28c061297f
Luz Dary Dary <21940802-Luz-DaryDary2@users.noreply.replit.com>
2024-03-13 13:06:03 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// axios npm install react-dropzone axios
+// Utilizar el componente UploadImage en la función principal
+import React from 'react';
+import UploadImage from './Components/UploadImage';
+
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <UploadImage />
+      React ⚛️ + Vite ⚡ + Replit :D
+    </main>
+  )
+}
+
+/*import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit
+    </main>
+  )
+}
+*/
